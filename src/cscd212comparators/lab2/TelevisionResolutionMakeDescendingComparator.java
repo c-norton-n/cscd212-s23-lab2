0d48b76dfa057527b488ddf5cd7bab7d6f5e5291
Cameron Norton <cnortonn@ewu.edu>
2023-04-05 20:27:12 -0700

    Add null check to TelevisionResolutionMakeDescendingComparator.
    Also flip the direction because I got it backwards.

--- src/cscd212comparators/lab2/TelevisionResolutionMakeDescendingComparator.java
+++ src/cscd212comparators/lab2/TelevisionResolutionMakeDescendingComparator.java
@@ -6,9 +6,12 @@ import cscd212classes.lab2.Television;
 public class TelevisionResolutionMakeDescendingComparator
     implements Comparator<Television> {
     public int compare(final Television t1, final Television t2) {
+        if (t1 == null || t2 == null)
+            throw new IllegalArgumentException
+                ("null parameter in TelevisionResolutionMakeDescendingComparator");
         if (t1.getResolution() == t2.getResolution()) {
-            return t1.getMake().compareTo(t2.getMake());
+            return t2.getMake().compareTo(t1.getMake());
         }
-        return t1.getResolution() - t2.getResolution();
+        return t2.getResolution() - t1.getResolution();
     }
 }
