b161a3888909723fabdc6e56b6b793b2808c97a6
Cameron Norton <cnortonn@ewu.edu>
2023-04-05 20:27:47 -0700

    Add null check to TelevisionScreenSizeComparator.

--- src/cscd212comparators/lab2/TelevisionScreenSizeComparator.java
+++ src/cscd212comparators/lab2/TelevisionScreenSizeComparator.java
@@ -5,6 +5,9 @@ import cscd212classes.lab2.Television;
 
 public class TelevisionScreenSizeComparator implements Comparator<Television> {
     public int compare(final Television t1, final Television t2) {
+        if (t1 == null || t2 == null)
+            throw new IllegalArgumentException
+                ("null parameter in TelevisionScreenSizeComparator");
         return t1.getScreenSize() - t2.getScreenSize();
     }
 }
